a956c54c25039ba3539cb7a6929d769261000e9e
abduraimovdev <kenzo778118@gmail.com>
2024-05-20 11:35:31 +0500

    ....

--- bin/server/service.dart
+++ bin/server/service.dart
@@ -5,12 +5,12 @@ import 'api/api.dart';
 
 /// Class used to set up all the routing for your server
 class Service {
-  final _swagger = SwaggerUI('swagger.yaml', title: 'Swagger Test');
+  // final _swagger = SwaggerUI('swagger.yaml', title: 'Swagger Test');
   Handler get handler {
     // Router
     final router = Router();
 
-    router.mount('/swagger', _swagger.call);
+    // router.mount('/swagger', _swagger.call);
     router.mount('/api/', Api().router.call);
     // router.get('/swagger/', _swagger.call);
     // Other End point block
