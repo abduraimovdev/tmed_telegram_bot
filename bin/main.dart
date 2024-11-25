7e7a5ee88167a7549a1a9977e85409440a434c56
abduraimovdev <kenzo778118@gmail.com>
2024-11-25 17:05:41 +0500

    nimadir muammo bor

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,7 @@
 import 'package:dotenv/dotenv.dart';
-
+import './log_service/log_service.dart';
 import './tmed_bot/main.dart';
 import './sq_bot/main.dart';
-import './log_service/log_service.dart';
 
 final env = DotEnv(includePlatformEnvironment: true)..load();
 void main(List args) async {
