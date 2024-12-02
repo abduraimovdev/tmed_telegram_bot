7defa4d8df975706ce457aa4b1d42e431bc7c860
Kenzo <kenzo778118@gmail.com>
2024-12-02 11:16:04 +0500

    Update code

--- bin/main.dart
+++ bin/main.dart
@@ -5,11 +5,21 @@ import './sq_bot/main.dart';
 
 final env = DotEnv(includePlatformEnvironment: true)..load();
 void main(List args) async {
-  print(args);
-  print("--------------------------------------------");
-  print(env['host']);
-  print("--------------------------------------------");
-  await LogBot.init();
-  mainTmed(args);
-  mainSQ(args);
+  try {
+    print(args);
+    print("--------------------------------------------");
+    print(env['host']);
+    print("--------------------------------------------");
+    await LogBot.init();
+    mainTmed(args);
+    mainSQ(args);
+  } catch (e, s) {
+    print(
+        "-----------------------------ERROR---------------------------------");
+    print(e);
+    print(s);
+    print(
+        "-----------------------------ERROR END---------------------------------");
+    main(args);
+  }
 }
