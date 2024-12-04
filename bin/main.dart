2a7c1ce7424bb8b9b561187812c99ecdbee5b725
Kenzo <kenzo778118@gmail.com>
2024-12-04 13:10:24 +0500

    Refresh

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,29 @@
+import 'dart:io';
+
 import 'package:dotenv/dotenv.dart';
 import './log_service/log_service.dart';
 import './tmed_bot/main.dart';
 import './sq_bot/main.dart';
 
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 final env = DotEnv(includePlatformEnvironment: true)..load();
 void main(List args) async {
+  while(true) {
+    runBot();
+  }
+}
+
+
+
+void runBot() {
   try {
     print(args);
     print("--------------------------------------------");
